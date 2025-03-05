7ee507dc245c890f0704930d71aee2a88c23110a
Esther <esther@fluna.co>
2025-03-05 13:27:10 +0100

    Complete custom button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
-
 import CustomButton from "@/components/Button";
+import { Delete } from "@mui/icons-material";
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to My Next.js App</h1>
-      <CustomButton label="Click Me" />
     </div>
   );
 }
